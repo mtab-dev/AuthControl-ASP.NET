d6aeb8929ba6c2e268157e59da2e8e1dd5c7a127
matheus abrantes <abranteselcio@gmail.com>
2025-02-27 13:02:28 -0300

    chore: add db context

---
+++ AuthControl.API/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using AuthControl.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthControl.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Models.User> Users { get; set; }
+        public DbSet<Models.Role> Roles { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Role)
+                .WithMany(r => r.Users)
+                .HasForeignKey(u => u.RoleId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
